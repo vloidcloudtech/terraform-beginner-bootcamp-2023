66ade6b3fd1ed37acb65db769778d4130c7a9d95
vloidcloudtech <jcwalker026@gmail.com>
2023-10-06 23:35:44 +0000

    #25 enable static website hosting, uploaded index/error file in terraform

--- modules/terrahouse_aws/variables.tf
+++ modules/terrahouse_aws/variables.tf
@@ -16,3 +16,25 @@ variable "bucket_name" {
     error_message = "Invalid bucket name. Bucket name must start and end with a lowercase letter or number, and can contain only lowercase letters, numbers, and hyphens. It must be between 3 and 63 characters in length."
   }
 }
+
+variable "index_html_filepath" {
+  description = "The file path for index.html"
+  type = string
+
+  validation {
+    condition = fileexists(var.index_html_filepath)
+    error_message = "The provided path for index.html does not exist"
+  }
+  
+}
+
+variable "error_html_filepath" {
+  description = "The file path for error.html"
+  type = string
+
+  validation {
+    condition = fileexists(var.error_html_filepath)
+    error_message = "The provided path for index.html does not exist"
+  }
+  
+}
