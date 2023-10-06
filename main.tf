aa0103618029ec384425b07cd6c29f9f352d8f09
vloidcloudtech <115376153+vloidcloudtech@users.noreply.github.com>
2023-10-06 16:29:49 -0400

    #23 refactor s3 bucket into the new terrahouse_aws module (#24)

--- main.tf
+++ main.tf
@@ -1,15 +1,15 @@
+terraform {
+#cloud{
+#  organization = "vloid"
+#
+ # workspaces {
+#    name = "terra-house-1"
+#  }
+#}
+}
 
-
- # https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/s3_bucket
-
-
-resource "aws_s3_bucket" "website_bucket" {
-#Bucket Naming Rules
- #https://docs.aws.amazon.com/AmazonS3/latest/userguide/bucketnamingrules.html?icmpid=docs_amazons3_console \
-  bucket = var.bucket_name
-
-    tags = {
-    UserUuid = var.user_uuid
-    #Environment = "Dev"
-  }
+module "terrahouse_aws" {
+  source = "./modules/terrahouse_aws"
+  user_uuid = var.user_uuid
+  bucket_name = var.bucket_name
 }
