1b912e9af00f3e997b54d5749d13a75dd27dc0f5
vloidcloudtech <115376153+vloidcloudtech@users.noreply.github.com>
2023-10-11 12:02:05 -0400

    #48 fix issues with provider, set up towns (#49)

--- variables.tf
+++ variables.tf
@@ -1,28 +1,25 @@
 variable "terratowns_endpoint" {
-  type        = string
+ type = string
 }
+
 variable "terratowns_access_token" {
-  type        = string
+ type = string
 }
+
 variable "teacherseat_user_uuid" {
-  type        = string
+ type = string
 }
 
-variable "bucket_name" {
-  type        = string
+variable "kakashi" {
+  type = object({
+    public_path = string
+    content_version = number
+  })
 }
 
-variable "index_html_filepath" {
-  type = string
-}
-
-variable "error_html_filepath" {
-  type = string
-}
-variable "content_version" {
-  type        = number
-}
-variable "assets_path" {
-  description = "Path to assets folder"
-  type        = string
+variable "soul" {
+  type = object({
+    public_path = string
+    content_version = number
+  })
 }
